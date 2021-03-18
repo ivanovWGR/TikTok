08b619cb2cf8415c6bcee70e085b2697509eba85
Borislav Ivanov <borislavivanovtw@abv.bg>
2021-03-19 01:22:20 +0200

    testCommit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <>
       <h1 className='center'>Let's Start</h1>
       <h3 className='center'>Tik-Tok implementation</h3>
+      <p className='center' style={{ color: 'grey' }}>First react App</p>
       <br />
       <hr />
     </>
