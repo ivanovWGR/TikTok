a604bcc8e68b92d59739609868bd3f691f94e340
Borislav Ivanov <borislavivanovtw@abv.bg>
2021-03-19 01:14:32 +0200

    InitiaCommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='center'>Let's Start</h1>
+      <h3 className='center'>Tik-Tok implementation</h3>
+      <br />
+      <hr />
+    </>
   );
 }
 
