2fc652a7bc215fd4e2e35f59e01a1e78b792d70f
Kiril Zashev <k.y.zashev@gmail.com>
2021-03-25 21:05:08 +0200

    Login dialog boxes and login/reg form are now modal, called from login button in header

--- src/HeaderComponents/HeaderRightDivUserNot.js
+++ src/HeaderComponents/HeaderRightDivUserNot.js
@@ -1,19 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Modal } from 'antd'
+import LoginPage from "../LoginPage/LoginPage"
+import styles from '../LoginPage/Login.module.css'
 import { HiOutlineDotsVertical } from "react-icons/hi";
 
 
 function HeaderRightDivUserNot() {
+
+    const [isModalShown, showModal] = useState(false)
+    function bringModal() {
+        showModal(true)
+    }
+    function closeModal() {
+        showModal(false)
+    }
+
     return (
         <div id="headerRightWrapper">
             <button id="upload" >
                 <span>Upload</span></button>
-            <button id="loginBtn">Log in</button>
+            <button id="loginBtn" onClick={bringModal}>Log in</button>
+            <Modal
+                // title="Basic Modal"
+                visible={isModalShown}
 
-            <button id="dotsMenu">
+                onCancel={closeModal}
+                okButtonProps={{ disabled: false }}
+                cancelButtonProps={{ disabled: false }}
+                footer ={null}
+                // className = {styles.loginPageRooter}
+                mask={true}
+                // width= "500px"
+                
+            >
+                
+                <LoginPage />
+                
+                
+                
+            </Modal>
+
+            {/* <button id="dotsMenu">
                 <HiOutlineDotsVertical />
-            </button>
+            </button> */}
         </div>
     )
 }
+
+
+// class App extends React.Component {
+// state = { visible: false };
+
+// showModal = () => {
+//     this.setState({
+//         visible: true,
+//     });
+// };
+
+// handleOk = e => {
+//     console.log(e);
+//     this.setState({
+//         visible: false,
+//     });
+
+
+// handleCancel = e => {
+//     console.log(e);
+//     this.setState({
+//         visible: false,
+//     });
+// };
+
+//     render() {
+//         return (
+//             <>
+//                 <Button type="primary" onClick={this.showModal}>
+//                     Open Modal with customized button props
+//         </Button>
+
+//             </>
+//         );
+//     }
+// }
+
+
 
 export default HeaderRightDivUserNot
