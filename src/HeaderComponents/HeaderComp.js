cc9731e4c0099d82fdb02013f7e147607edafd09
Kiril Zashev <k.y.zashev@gmail.com>
2021-03-26 16:48:16 +0200

    search input replaced, value taken, styles and icons max close to original site

--- src/HeaderComponents/HeaderComp.js
+++ src/HeaderComponents/HeaderComp.js
@@ -1,11 +1,16 @@
 import { FaSearch } from "react-icons/fa";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 import HeaderRightDivUserNot from "./HeaderRightDivUserNot"
 import HeaderRightDivUserYes from "./HeaderRightDivUserYes"
 import styles from './Header.module.scss'
+import { Input, Space } from 'antd';
+
+
+const { Search } = Input;
 
 export default function HeaderComp({ isUserLoggedIn }) {
+    const onSearch = value => console.log(value);
     return (
         <div className={styles.header}>
             <span>
@@ -14,11 +19,17 @@ export default function HeaderComp({ isUserLoggedIn }) {
                     <img src="https://sf16-scmcdn-va.ibytedtos.com/goofy/tiktok/web/node/_next/static/images/logo-text-dark-673b189595b95d8bbf2ab1783ae2ab25.svg" alt="sign" />
                 </Link>
             </span>
-            <form>
-                <input type="text" placeholder="Search accounts"></input><span>|</span>
-                <button type="submit"><FaSearch /></button>
+            <form>                
+                <Space direction="vertical">
+                    <Search className={styles.searchInputContainer}
+                        id={styles.searchInput}
+                        placeholder="Search accounts"
+                        allowClear onSearch={onSearch}
+                        bordered={false}
+                    />
+                </Space>
             </form>
-            {isUserLoggedIn ?  <HeaderRightDivUserYes />: <HeaderRightDivUserNot /> }
+            {isUserLoggedIn ? <HeaderRightDivUserYes /> : <HeaderRightDivUserNot />}
         </div>
 
 
