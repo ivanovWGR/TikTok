b5e134dda6381ab0978ed0ea0478dfa967542983
Borislav Ivanov <borislavivanovtw@abv.bg>
2021-03-29 23:58:05 +0300

    /

--- src/HeaderComponents/HeaderRightDivUserYes.js
+++ src/HeaderComponents/HeaderRightDivUserYes.js
@@ -1,15 +1,13 @@
-import React, {useState}  from 'react'
+import React, { useState } from 'react'
 
 import { RiUploadCloudLine, RiSettings5Line } from "react-icons/ri"
 import { BiMessageAltMinus, BiUser, BiHelpCircle } from "react-icons/bi"
 import { CgProfile } from "react-icons/cg"
-import {FiLogIn} from "react-icons/fi"
-import {GrLanguage} from "react-icons/gr"
-import { Tooltip} from "antd"
+import { FiLogIn } from "react-icons/fi"
+import { GrLanguage } from "react-icons/gr"
+import { Tooltip } from "antd"
 import { formatCountdown } from 'antd/lib/statistic/utils';
-import {Link} from 'react-router-dom';
-
-
+import { Link } from 'react-router-dom';
 
 
 
@@ -19,15 +17,15 @@ function HeaderRightDivUserYes() {
         <div id="headerRightWrapperUserOnline">
 
             <Link to='/upload'>
-            <button className="userLoggedInSideMenu" >
-                <Tooltip title="Upload video"
-                    placement="bottom"
-                    id="tooltip">
-                    <RiUploadCloudLine />
-                </Tooltip>
-            </button>
+                <button className="userLoggedInSideMenu" >
+                    <Tooltip title="Upload video"
+                        placement="bottom"
+                        id="tooltip">
+                        <RiUploadCloudLine />
+                    </Tooltip>
+                </button>
             </Link>
-            
+
 
 
             <button className="userLoggedInSideMenu" >
@@ -39,20 +37,20 @@ function HeaderRightDivUserYes() {
             </button>
 
             <span className="profileIconSpan" id="profileIconSpan"
-            onMouseEnter = {()=> setIsShown(true)}
-            onMouseLeave = {()=> setIsShown(false)}
+                onMouseEnter={() => setIsShown(true)}
+                onMouseLeave={() => setIsShown(false)}
             ><CgProfile /></span>
 
             {isShown && <div className="navHoverMenu"
-            onMouseEnter = {()=>setIsShown(true)}
-            onMouseLeave = {()=> setIsShown(false)}
+                onMouseEnter={() => setIsShown(true)}
+                onMouseLeave={() => setIsShown(false)}
             >
-                <div><a href="#"> <span> <BiUser/> </span> <span>View profile</span> </a> </div>
-                <div><a href="#"> <span> <RiSettings5Line/></span> Settings</a></div>
-                <div><a href="#"><span> <GrLanguage/></span> Language</a></div>
-                <div><a href="#"> <span> <BiHelpCircle/></span> Feeback and help</a></div>
-                <div id="logOutDiv"><a href="#"><span> <FiLogIn/></span> Log out </a></div>
-            </div>}           
+                <div><a href="#"> <span> <BiUser /> </span> <span>View profile</span> </a> </div>
+                <div><a href="#"> <span> <RiSettings5Line /></span> Settings</a></div>
+                <div><a href="#"><span> <GrLanguage /></span> Language</a></div>
+                <div><a href="#"> <span> <BiHelpCircle /></span> Feeback and help</a></div>
+                <div id="logOutDiv"><a href="#"><span> <FiLogIn /></span> Log out </a></div>
+            </div>}
 
         </div>
     )
